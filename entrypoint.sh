07aa2288f89e9ab68d9a61e7605deb3f34803de8
Allgot <dlwlstj496@gmail.com>
2022-10-21 12:37:12 +0900

    Docker container action uses another enviroments, so cannot use initiated shell immediately.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,9 @@
 #!/bin/bash -l
 
-# opam init --yes
-# eval $(opam env)
+opam init --yes
+eval $(opam env)
 # opam install dune
-
-# eval $(opam env)
 
 # dune build ocaml_test.exe
 
-opam switch
-# dune exec ./ocaml_test.exe $1 $2 
+dune exec ./ocaml_test.exe $1 $2 
