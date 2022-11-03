6bc770da0d276bc06d99981fd1f917b143746301
Allgot <dlwlstj496@gmail.com>
2022-11-03 21:47:33 +0900

    Now compare all possible pairs.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,28 @@
 #!/bin/bash -l
 
-opam init --yes
-eval $(opam env)
-opam install dune --yes
-opam install cohttp-lwt-unix --yes --confirm-level=unsafe-yes
-opam install lwt_ssl --yes --confirm-level=unsafe-yes
-opam install yojson --yes --confirm-level=unsafe-yes
+# opam init --yes
+# eval $(opam env)
+# opam install dune --yes
+# opam install cohttp-lwt-unix --yes --confirm-level=unsafe-yes
+# opam install lwt_ssl --yes --confirm-level=unsafe-yes
+# opam install yojson --yes --confirm-level=unsafe-yes
+
+# eval $(opam env) # 제대로 못 잡아주는중.....
+
+env CAML_LD_LIBRARY_PATH=/root/.opam/default/lib/stublibs:/root/.opam/default/lib/ocaml/stublibs:/root/.opam/default/lib/ocaml > /dev/null
+env OCAML_TOPLEVEL_PATH=/root/.opam/default/lib/toplevel > /dev/null
+env PKG_CONFIG_PATH=/root/.opam/default/lib/pkgconfig > /dev/null
+env HOSTNAME=32638fc6e417 > /dev/null
+env PWD=/ > /dev/null
+env MANPATH=:/root/.opam/default/man > /dev/null
+env OPAM_SWITCH_PREFIX=/root/.opam/default > /dev/null
+env HOME=/root > /dev/null
+env LESSCLOSE=/usr/bin/lesspipe %s %s > /dev/null
+env TERM=xterm > /dev/null
+env LESSOPEN=| /usr/bin/lesspipe %s > /dev/null
+env OLDPWD=/roote > /dev/null
+PATH=/root/.opam/default/bin:$PATH > /dev/null
 
 # dune build ocaml_test.exe
 
-dune exec ./ocaml_test.exe $1 $2 $3 # $1: issue_num, $2: issue_contents, $3: repository_url
+dune exec ./ocaml_test.exe $1 "$2" $3 $4 # $1: issue_num, $2: issue_contents, $3: repository_path_name, $4: api_key
--- ocaml_test.ml
+++ ocaml_test.ml
@@ -2,52 +2,49 @@
     let issue_contents = Sys.argv.(2) in
         let _ = (Printf.printf "%s and %s\n" issue_num issue_contents) in
             Printf.printf "::set-output name=string::%s\n" issue_contents *)
-            open Lwt
-            open Cohttp
-            open Cohttp_lwt_unix
-            
-            (* open Yojson *)
-            
+open Lwt
+open Cohttp
+open Cohttp_lwt_unix
+
+(* open Yojson *)
+
+let body =
+    Client.get ~headers:(Cohttp.Header.init_with "accept" "application/vnd.github+json") (Uri.of_string ("https://api.github.com/repos/" ^ Sys.argv.(3) ^ "/issues?state=all")) >>= fun (resp, body) ->
+        let code = resp |> Response.status |> Code.code_of_status in
+            Printf.printf "Response code: %d\n" code;
+            Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string);
+        Cohttp_lwt.Body.to_string body
+
+let issue_list = 
+    let body = Lwt_main.run body in
+        let json_body = Yojson.Basic.from_string body in
+            let open Yojson.Basic.Util in
+                let extract_issues (json: Yojson.Basic.t) : string list =
+                    [json]
+                        (* |> to_list *)
+                        |> flatten
+                        |> filter_member "body"
+                        |> filter_string in
+                    (* let body_list = extract_issues json_body in
+                        List.iter (print_endline) body_list *)
+                        extract_issues json_body
+
+let sim_header = Cohttp.Header.of_list [("X-RapidAPI-Key", Sys.argv.(4)); ("X-RapidAPI-Host", "twinword-text-similarity-v1.p.rapidapi.com"); ("content-type", "application/x-www-form-urlencoded")]
+
+let () = List.iter (fun issue_contents -> 
+    let text1 = Yojson.Basic.to_string (`String Sys.argv.(2)) in
+    let text2 = Yojson.Basic.to_string (`String issue_contents) in
+    let () = Printf.printf "Compare %s with %s\n" text1 text2 in
+        if text1 != text2 then
             let body =
-                Client.get ~headers:(Cohttp.Header.init_with "accept" "application/vnd.github+json") (Uri.of_string ("https://api.github.com/repos/prosyslab/dude-test/issues?state=all")) >>= fun (resp, body) ->
-                    let code = resp |> Response.status |> Code.code_of_status in
-                        Printf.printf "Response code: %d\n" code;
-                        Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string);
-                    Cohttp_lwt.Body.to_string body
-            
-            let issue_list = 
-                let body = Lwt_main.run body in
-                    let json_body = Yojson.Basic.from_string body in
-                        let open Yojson.Basic.Util in
-                            let extract_issues (json: Yojson.Basic.t) : string list =
-                                [json]
-                                    (* |> to_list *)
-                                    |> flatten
-                                    |> filter_member "body"
-                                    |> filter_string in
-                                (* let body_list = extract_issues json_body in
-                                    List.iter (print_endline) body_list *)
-                                    extract_issues json_body
-            
-            let sim_header = Cohttp.Header.of_list [("X-RapidAPI-Key", "your_key_here"); ("X-RapidAPI-Host", "twinword-text-similarity-v1.p.rapidapi.com"); ("content-type", "application/x-www-form-urlencoded")]
-            
-            let () = List.iter (fun issue_contents -> 
-                let text1 = Yojson.Basic.to_string (`String Sys.argv.(2)) in (* Sys.argv.(2) *)
-                let text2 = Yojson.Basic.to_string (`String issue_contents) in
-                let body =
-                    Client.get  ~headers:sim_header (Uri.of_string ("https://twinword-text-similarity-v1.p.rapidapi.com/similarity/?" ^ "text1=" ^ text1 ^ "&" ^ "text2=" ^ text2)) >>= fun (_, body) ->
-                        (* let code = resp |> Response.status |> Code.code_of_status in *)
-                            (* Printf.printf "Response code: %d\n" code; *)
-                            (* Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string); *)
-                        Cohttp_lwt.Body.to_string body in
-                
-            
-                let body = Lwt_main.run body in
-                    let json_body = Yojson.Basic.from_string body in
-                        let open Yojson.Basic.Util in
-                            Printf.printf "Similarity: %s\n" (Float.to_string (List.hd ([json_body] |> filter_member "similarity" |> filter_number)))
-            ) issue_list
-            
-            
-            
-            
+                Client.get  ~headers:sim_header (Uri.of_string ("https://twinword-text-similarity-v1.p.rapidapi.com/similarity/?" ^ "text1=" ^ text1 ^ "&" ^ "text2=" ^ text2)) >>= fun (_, body) ->
+                    (* let code = resp |> Response.status |> Code.code_of_status in *)
+                        (* Printf.printf "Response code: %d\n" code; *)
+                        (* Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string); *)
+                    Cohttp_lwt.Body.to_string body in
+
+    let body = Lwt_main.run body in
+        let json_body = Yojson.Basic.from_string body in
+            let open Yojson.Basic.Util in
+                Printf.printf "- Similarity: %s\n" (Float.to_string (List.hd ([json_body] |> filter_member "similarity" |> filter_number)))
+) issue_list
