3408d7c96855997d9ae4e1b6ff9402f3fb46a680
Allgot <96973603+Allgot@users.noreply.github.com>
2022-11-21 14:04:33 +0900

    Updated to return the possible duplication number.

--- ocaml_test.ml
+++ ocaml_test.ml
@@ -19,7 +19,7 @@ let rec get_issues page_num res =
                     |> filter_string,
                  [body |> Yojson.Basic.from_string]
                     |> flatten
-                    |> fliter_member "number"
+                    |> filter_member "number"
                     |> filter_number) in
 
     let issue_list = fst issue_tup in
@@ -32,6 +32,10 @@ let rec get_issues page_num res =
 
 let sim_header = Cohttp.Header.of_list [("X-RapidAPI-Key", Sys.argv.(4)); ("X-RapidAPI-Host", "twinword-text-similarity-v1.p.rapidapi.com"); ("content-type", "application/x-www-form-urlencoded")]
 
+let threshold_sim = 0.20
+let max_sim = ref 0.0
+let max_contents = ref ""
+
 let () = List.iter (fun issue_contents -> 
     let text1 = Yojson.Basic.to_string (`String Sys.argv.(2)) in
     let text2 = Yojson.Basic.to_string (`String issue_contents) in
@@ -39,13 +43,15 @@ let () = List.iter (fun issue_contents ->
         let () = Printf.printf "Compare %s with %s\n" text1 text2 in
             let body =
                 Client.get  ~headers:sim_header (Uri.of_string ("https://twinword-text-similarity-v1.p.rapidapi.com/similarity/?" ^ "text1=" ^ text1 ^ "&" ^ "text2=" ^ text2)) >>= fun (_, body) ->
-                    (* let code = resp |> Response.status |> Code.code_of_status in *)
-                        (* Printf.printf "Response code: %d\n" code; *)
-                        (* Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string); *)
                     Cohttp_lwt.Body.to_string body in
 
     let body = Lwt_main.run body in
         let json_body = Yojson.Basic.from_string body in
             let open Yojson.Basic.Util in
-                Printf.printf "- Similarity: %s\n" (Float.to_string (List.hd ([json_body] |> filter_member "similarity" |> filter_number)))
+                let cur_sim = List.hd ([json_body] |> filter_member "similarity" |> filter_number)) in
+                    if cur_sim > threshold_sim && cur_sim > !max_sim then
+                        let _ = max_sim := cur_sim in
+                        max_contents := issue_contents
 ) (get_issues 0 [])
+
+let _ = Printf.printf "::set-output name=dup_num::%d\n" (ConNum.find !max_contents !map_ConNum)
