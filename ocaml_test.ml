74a3f18eb10afc12d15fb34a388ba75f3bc518d3
Allgot <dlwlstj496@gmail.com>
2022-11-21 11:46:54 +0900

    Added mapping from contents to the issue number.

--- ocaml_test.ml
+++ ocaml_test.ml
@@ -1,18 +1,31 @@
 open Lwt
 open Cohttp_lwt_unix
 
+module ConNum = Map.Make(String)
+
+let mapConNum = ConNum.empty
+
 let rec get_issues page_num res =
     let body =
         Client.get ~headers: (Cohttp.Header.init_with "accept" "application/vnd.github+json") (Uri.of_string ("https://api.github.com/repos/" ^ Sys.argv.(3) ^ "/issues?state=all&per_page=100&" ^ "page=" ^ (Int.to_string page_num))) >>= fun (_, body) ->
             Cohttp_lwt.Body.to_string body in
     
-    let issue_list =
+    let issue_tup =
         let body = Lwt_main.run body in
             let open Yojson.Basic.Util in
-                [body |> Yojson.Basic.from_string]
+                ([body |> Yojson.Basic.from_string]
                     |> flatten
                     |> filter_member "body"
-                    |> filter_string in
+                    |> filter_string,
+                 [body |> Yojson.Basic.from_string]
+                    |> flatten
+                    |> fliter_member "number"
+                    |> filter_number) in
+
+    let issue_list = fst issue_tup in
+    let num_list = snd issue_tup in
+
+    let _ = List.iter2 (fun content num -> ConNum.add content num mapConNum) issue_list num_list in 
 
     if List.length issue_list == 0 then []
     else issue_list @ (get_issues (page_num+1) res)
